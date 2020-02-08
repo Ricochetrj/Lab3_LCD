2349e33ac68f49cd73997993a7370cabcab772b2
Ricochetrj <38703824+Ricochetrj@users.noreply.github.com>
2020-02-07 23:53:36 -0600

    Inicio de Libreria

--- Lab3_LCD.X/LCD_prog.c
+++ Lab3_LCD.X/LCD_prog.c
@@ -28,90 +28,15 @@
 
 #include <xc.h>
 #include <stdint.h>
-//#include "LCDFunctions.h"
 
-/* Configure the data bus and Control bus as per the hardware connection 
-   Dtatus bus is connected to PB0:PB7 and control bus PD0:PD2*/
-#define LcdDataBus      PORTB
-#define LcdControlBus   PORTD
+#define LCD_E PORTDbits.RD6
+#define LCD_RS PORTDbits.RD7
+#define LCD_Data PORTB
 
-#define LcdDataBusDirnReg   TRISB
-#define LcdCtrlBusDirnReg   TRISD
+#define LCD_E_Dir TRISDbits.TRISD6
+#define LCD_RS_Dir TRISDbits.TRISD7
+#define LCD_Data_IO TRISB
 
-#define LCD_RS     0
-#define LCD_RW     1
-#define LCD_EN     2
-
-
-/* local function to generate delay */
-void delay(int cnt)
-{
-    int i;
-    for(i=0;i<cnt;i++);
-}
-
-
-/* Function to send the command to LCD */
-void Lcd_CmdWrite(char cmd)
-{
-    LcdDataBus = cmd;               //Send the Command nibble
-    LcdControlBus &= ~(1<<LCD_RS);  // Send LOW pulse on RS pin for selecting Command register
-    LcdControlBus &= ~(1<<LCD_RW);  // Send LOW pulse on RW pin for Write operation
-    LcdControlBus |= (1<<LCD_EN);   // Generate a High-to-low pulse on EN pin
-    delay(100);
-    LcdControlBus &= ~(1<<LCD_EN); 
-
-    delay(10000);
-}
-
-
-
-/* Function to send the Data to LCD */
-void Lcd_DataWrite(char dat)
-{
-    LcdDataBus = dat;               //Send the data on DataBus nibble
-    LcdControlBus |= (1<<LCD_RS);   // Send HIGH pulse on RS pin for selecting data register
-    LcdControlBus &= ~(1<<LCD_RW);  // Send LOW pulse on RW pin for Write operation
-    LcdControlBus |= (1<<LCD_EN);   // Generate a High-to-low pulse on EN pin
-    delay(100);
-    LcdControlBus &= ~(1<<LCD_EN);
-
-    delay(10000);
-}
-
-
-
-int main()
-{
-    char i,a[]={"Good morning!"};
-
-    LcdDataBusDirnReg = 0x00;  // Configure all the LCD pins as output
-    LcdCtrlBusDirnReg = 0x00;  // Configure the Ctrl pins as output
-
-
-    Lcd_CmdWrite(0x38);        // enable 5x7 mode for chars 
-    Lcd_CmdWrite(0x0E);        // Display OFF, Cursor ON
-    Lcd_CmdWrite(0x01);        // Clear Display
-    Lcd_CmdWrite(0x80);        // Move the cursor to beginning of first line
-
-
-    Lcd_DataWrite('H');
-    Lcd_DataWrite('e');
-    Lcd_DataWrite('l');
-    Lcd_DataWrite('l');
-    Lcd_DataWrite('o');
-    Lcd_DataWrite(' ');
-    Lcd_DataWrite('w');
-    Lcd_DataWrite('o');
-    Lcd_DataWrite('r');
-    Lcd_DataWrite('l');
-    Lcd_DataWrite('d');
-
-    Lcd_CmdWrite(0xc0);        //Go to Next line and display Good Morning
-    for(i=0;a[i]!=0;i++)
-    {
-        Lcd_DataWrite(a[i]);
-    }
-
-    while(1);
+void main(void){
+    
 }

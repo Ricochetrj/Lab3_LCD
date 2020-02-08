b51f327f625b1a0356c99fb7cb37221b4af0ef18
Ricochetrj <38703824+Ricochetrj@users.noreply.github.com>
2020-02-08 00:15:52 -0600

    Se agrega una funcion de variable
    send 'yea' to LCD

--- Lab3_LCD.X/LCD_prog.c
+++ Lab3_LCD.X/LCD_prog.c
@@ -37,6 +37,72 @@
 #define LCD_RS_Dir TRISDbits.TRISD7
 #define LCD_Data_IO TRISB
 
+#define _XTAL_FREQ 4000000
+void PulsoE(void);
+void WriteCMLCD(unsigned char CM);
+void WriteDataLCD( char msg);
+void initLCD(void);
+void StringLCD(const char *letras );
+void ClearScreenLCD(void);
+const char *yea;
 void main(void){
+    initLCD();
+    StringLCD(yea);
     
+    
+}
+void PulsoE(void){
+    LCD_E = 1;
+    __delay_us(10);
+    LCD_E = 0;
+    __delay_us(10);
+}
+
+void WriteCMLCD(unsigned char CM){
+    LCD_RS=0; // Identifica que se le esta mandando un comando
+    LCD_Data = CM; // Mandar comando a la LCD
+    PulsoE();
+}
+
+void WriteDataLCD(char msg ){
+    LCD_RS = 1;
+    LCD_Data = msg;
+    PulsoE();
+}
+
+void initLCD(void){
+   LCD_E = 0;// Inicializar variables y puertos
+   LCD_RS = 0;
+   LCD_Data = 0;
+   LCD_E_Dir = 0;
+   LCD_RS_Dir = 0;
+   LCD_Data_IO = 0;
+    
+    __delay_ms(40);
+    WriteCMLCD(0x30);
+    
+    __delay_ms(6);
+    WriteCMLCD(0x30);
+    
+    __delay_us(300);
+    WriteCMLCD(0x30);
+    
+    __delay_ms(2);
+    
+    WriteCMLCD(0x38); //Se manda la funcion
+    WriteCMLCD(0x0C); //Encender Display, Apagar cursor, Apagar Parpadeo
+    WriteCMLCD(0x01); //Limpiar Display
+    WriteCMLCD(0x06); //Entry mode
+    
+}
+
+void StringLCD(const char *letras){ //Parametro Volatil
+    while(*letras){
+        WriteDataLCD(*letras ++);
+    }
+}
+
+void ClearScreenLCD(void){
+    WriteCMLCD(0x01);
+    __delay_ms(2);
 }
